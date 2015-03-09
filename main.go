6e0e0578564885adfb8741b87aad4bd88c840287
Marc Bluemner <marc.bluemner@gmail.com>
2015-03-09 04:23:01 +0100

    added rouing with mux

--- main.go
+++ main.go
@@ -1,22 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/russross/blackfriday"
 )
 
 func main() {
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	r := mux.NewRouter().StrictSlash(false)
+	r.HandleFunc("/", HomeHandler)
 
-	http.HandleFunc("/markdown", GenerateMarkdown)
-	http.Handle("/", http.FileServer(http.Dir("public")))
-	http.ListenAndServe(":"+port, nil)
+	// Posts collection
+	posts := r.Path("/posts").Subrouter()
+	posts.Methods("GET").HandlerFunc(PostsIndexHandler)
+	posts.Methods("POST").HandlerFunc(PostsCreateHandler)
+
+	// Posts singular
+	post := r.PathPrefix("/posts/{id}").Subrouter()
+	post.Methods("GET").Path("/edit").HandlerFunc(PostEditHandler)
+	post.Methods("PUT", "POST").HandlerFunc(PostUpdateHandler)
+	post.Methods("DELETE").HandlerFunc(PostDeleteHandler)
+
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", r)
+}
+
+func HomeHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "Home")
+}
+
+func PostsIndexHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "posts index")
+}
+
+func PostsCreateHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "posts create")
+}
+
+func PostsShowHandler(rw http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	fmt.Fprintln(rw, "showing post", id)
+}
+
+func PostUpdateHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post update")
+}
+
+func PostDeleteHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post delete")
+}
+
+func PostEditHandler(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(rw, "post edit")
 }
 
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
